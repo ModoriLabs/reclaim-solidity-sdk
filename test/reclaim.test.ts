5684f270ff9d18f7c24fa7883962d4265e661f55
qpzm <lhm1442@gmail.com>
2025-01-15 18:23:16 +0900

    test: twitter follower count

--- test/reclaim.test.ts
+++ test/reclaim.test.ts
@@ -82,6 +82,68 @@ describe('Reclaim Tests', () => {
       let { contract, user, superProofs, } = await loadFixture(proofsFixture)
       let x = await contract.connect(user).verifyProof(superProofs[1])
     })
+
+    it('should simulate proof verification - twitter follower count', async () => {
+      let { contract, owner } = await loadFixture(deployFixture)
+      // proxy address, host is not checked in Reclaim.sol now.
+      const witnesses = [{addr:"0x189027e3c77b3a92fd01bf7cc4e6a86e77f5034e", host:"api.twitter.com"}]
+      await contract.addNewEpoch(witnesses, 1)
+      await contract.fetchEpoch(1)
+      const proof = {
+        claimInfo: {
+          provider: 'http',
+          parameters: '{"method":"GET","responseMatches":[{"type":"regex","value":"followers_count\\":\\\\s*(?<followers_count>[\\\\d\\\\.]+)"}],"url":"https://api.twitter.com/2/users/by/username/anime_kaguya?user.fields=public_metrics"}',
+          context: '{"extractedParameters":{"followers_count":"473022"},"providerHash":"0x12700638dc2514ec6c1608afc479d979d05c545cf0da29d2434ca2dbbf533600"}',
+        },
+        signedClaim: {
+          claim: {
+            epoch: 1,
+            identifier: '0x9e19819f75fe9a5440fc89505225f27104e85ca9456cf71ce441131e08ccd2f3',
+            owner: '0xf9f25d1b846625674901ace47d6313d1ac795265',
+            timestampS: 1731312831,
+          },
+          signatures: [
+            // claim signature
+            '0x5303b18942e4a2f395afc9485def2f0cb1b0649ce957df508dcc607f6d539ec6747197c9495c84d1229ca336c00b37335f461bb21955be2294047e4553760d961b',
+            // result signature
+            // '0x597e918d26fbbad5e571871a54fa8e7c4858639086acc386aa3e129bbb38c45d77ff10f54518b32b5c54a4edf36cb1f8a28803843abb794121aeb0f6d921db951c',
+          ],
+        }
+      }
+
+      const tx = await contract.verifyProof(proof)
+      await tx.wait() // this function is not view, so return value is meaningless
+      const count = await contract.extractFieldFromContext(proof.claimInfo.context, '"followers_count":"')
+      expect(count).to.equal('473022');
+    })
+
+    it('should simulate proof verification - github qpzm', async () => {
+      let { contract, owner } = await loadFixture(deployFixture)
+      const witnesses = [{addr:"0x244897572368eadf65bfbc5aec98d8e5443a9072", host:"wss://witness.reclaimprotocol.org/ws"}]
+      await contract.addNewEpoch(witnesses, 1)
+      await contract.fetchEpoch(1)
+      const proof = {
+        claimInfo: {
+          provider: 'http',
+          parameters: '{"additionalClientOptions":{},"body":"","geoLocation":"","headers":{"user-agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 17_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Safari/604.1"},"method":"GET","paramValues":{"username":"qpzm"},"responseMatches":[{"invert":false,"type":"contains","value":"<span class=\\"color-fg-muted\\">({{username}})</span>"}],"responseRedactions":[{"jsonPath":"","regex":"<span\\\\ class=\\"color\\\\-fg\\\\-muted\\">\\\\((.*)\\\\)</span>","xPath":""}],"url":"https://github.com/settings/profile"}',
+          context: '{"contextAddress":"0x0","contextMessage":"","extractedParameters":{"username":"qpzm"},"providerHash":"0x74734677e529e5d823b3a2845799a908ab59d2afa9ac168c6fd5d57d1b0e319f"}',
+        },
+        signedClaim: {
+          claim: {
+            epoch: 1,
+            identifier: '0x38857161784f3a3ae27d411909ef5a55606901525e6390267a82f7bc141a3ba8',
+            owner: '0xe57ab8012af3becf06d18b679f4a48d0c0865294',
+            timestampS: 1728025483,
+          },
+          signatures: [
+            '0xc754c9acb205dfbf8b06c5bab3ae4083ed19a4cc3732c5da23f6e54f8f667cf7444b837576ce4b0005a0f00287b609ab8d61a252fb75e14f15c75addf9ecfdc41b'
+          ],
+        }
+      }
+
+      const result = await contract.verifyProof(proof)
+    })
+
     it('should simulate proof verification', async () => {
       let { contract, owner } = await loadFixture(deployFixture)
       const witnesses = [{addr:"0x244897572368eadf65bfbc5aec98d8e5443a9072",host:"https://reclaim-node.questbook.app"}]
@@ -90,37 +152,37 @@ describe('Reclaim Tests', () => {
       const proof = {
 
         claimInfo: {
-      
+
           context: '{"contextAddress":"0x0","contextMessage":"0098967F","providerHash":"0xeda3e4cee88b5cbaec045410a0042f99ab3733a4d5b5eb2da5cecc25aa9e9df1"}',
-      
+
           provider: 'http',
-      
+
           parameters: '{"body":"","geoLocation":"in","method":"GET","responseMatches":[{"type":"contains","value":"_steamid\\">Steam ID: 76561198155115943</div>"}],"responseRedactions":[{"jsonPath":"","regex":"_steamid\\">Steam ID: (.*)</div>","xPath":"id(\\"responsive_page_template_content\\")/div[@class=\\"page_header_ctn\\"]/div[@class=\\"page_content\\"]/div[@class=\\"youraccount_steamid\\"]"}],"url":"https://store.steampowered.com/account/"}'
-      
+
         },
-      
+
         signedClaim: {
-      
+
           claim: {
-      
+
             epoch: 1,
-      
+
             identifier: '0x930a5687ac463eb8f048bd203659bd8f73119c534969258e5a7c5b8eb0987b16',
-      
+
             owner: '0xef27fa8830a070aa6e26703be6f17858b61d3fba',
-      
+
             timestampS: 1712685785
-      
+
           },
-      
+
           signatures: [
-      
+
             '0xb246a05693f3e21a70eab5dfd5edc1d0597a160c82b8bf9e24d1f09f9dde9899154bb1672c1bf38193a7829e96e4ed09bc327657bf266e90451f6a90c8b45dfb1c'
-      
+
           ]
-      
+
         }
-      
+
       }
       const result = await contract.verifyProof(proof)
     })
